1ec806051ad504e70bc5da58fddb21899d7b0937
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-08-14 22:27:45 +0530

    feat: added query to get all jobs and mutation to apply for job

--- backend/graphql/resolvers/job.ts
+++ backend/graphql/resolvers/job.ts
@@ -1,6 +1,7 @@
 import { ApolloError } from "apollo-server-errors";
 import getLoggedInUserId from "../../middleware/getLoggedInUserId";
 import Job, { IJob } from "../../models/Job";
+import User from "../../models/User";
 
 interface JobInput {
   title: string;
@@ -51,6 +52,30 @@ const resolvers = {
 
       return res;
     },
+    async applyForJob(
+      _: unknown,
+      { jobId }: { jobId: string },
+      ctx: any
+    ): Promise<IJob> {
+      const loggedInUserId = getLoggedInUserId(ctx);
+      const userId = loggedInUserId?.userId;
+
+      if (!userId) {
+        throw new ApolloError("User not authenticated", "NOT_AUTHENTICATED");
+      }
+
+      const job = (
+        await Job.findByIdAndUpdate(
+          jobId,
+          { $push: { applicants: userId } },
+          { new: true }
+        )
+      )?.toObject() as IJob;
+
+      await User.findByIdAndUpdate(userId, { $push: { jobsApplied: jobId } });
+
+      return job;
+    },
   },
   Query: {
     async myJobs(_: unknown, args: {}, ctx: any): Promise<IJob[] | null> {
@@ -66,6 +91,30 @@ const resolvers = {
       const jobsAsObjects = myJobs.map((job) => job.toObject()) as IJob[];
 
       return jobsAsObjects;
+    },
+    async getAllJobs(
+      _: unknown,
+      { skill, minSalary }: { skill: string[]; minSalary: number },
+      ctx: any
+    ): Promise<IJob[] | null> {
+      const loggedInUserId = getLoggedInUserId(ctx);
+      const userId = loggedInUserId?.userId;
+
+      if (!userId) {
+        throw new ApolloError("User not authenticated", "NOT_AUTHENTICATED");
+      }
+
+      const query: any = {};
+
+      if (skill?.length > 0) {
+        query.tags = { $in: [skill] };
+      }
+
+      if (minSalary !== undefined) {
+        query.salaryPerHour = { $gte: minSalary };
+      }
+
+      return await Job.find(query).populate("postedBy").populate("applicants");
     },
   },
 };
