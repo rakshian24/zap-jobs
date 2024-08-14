5335a9f2ea76323c9b92926d4a58a8251dabea3f
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-08-14 11:01:20 +0530

    fix: package.json build command fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>ZapJobs | Zap into your dream job</h1>;
+  return (
+    <div>
+      <h1>ZapJobs</h1>
+      <h2>Zap into your dream job</h2>
+    </div>
+  );
 }
 
 export default App;
