84ff93a3decea46019970c074ea69ffe352ad6fe
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-08-15 12:11:13 +0530

    feat: made auth pages responsive

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,36 +1,54 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { ReactComponent as Flagman } from "../../assets/svgs/flagman.svg";
 import { ReactComponent as Logo } from "../../assets/svgs/logo.svg";
-import { colors } from "../../constants";
+import { colors, screenSize } from "../../constants";
 
 const Home = () => {
+  const isTablet = useMediaQuery(`(max-width:${screenSize.tablet})`);
+
   return (
     <Stack direction={"row"} height={"100%"}>
-      <Stack width={"50%"} px={6} py={3} bgcolor={colors.primary}>
-        <Box>
-          <Logo width="200px" />
-        </Box>
-        <Box
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          height={"100%"}
-        >
-          <Flagman width="100%" height={"500px"} />
-        </Box>
-      </Stack>
+      {!isTablet && (
+        <Stack width={"50%"} px={6} py={3} bgcolor={colors.primary}>
+          <Box>
+            <Logo width="200px" />
+          </Box>
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            height={"100%"}
+          >
+            <Flagman width="100%" height={"500px"} />
+          </Box>
+        </Stack>
+      )}
       <Stack
-        width={"50%"}
+        width={isTablet ? "100%" : "50%"}
         height={"100%"}
-        pl={10}
-        pr={20}
+        pl={isTablet ? 3 : 10}
+        pr={isTablet ? 3 : 20}
         display={"flex"}
-        justifyContent={"center"}
+        justifyContent={isTablet ? "flex-start" : "center"}
       >
-        <Box py={7.5}>
+        <Stack py={isTablet ? 0 : 7.5}>
+          {isTablet && (
+            <Stack>
+              <Logo width="200px" style={{ marginTop: "32px" }} />
+              <Typography
+                px={1.5}
+                fontSize={20}
+                mb={3}
+                fontWeight={500}
+                sx={{ fontStyle: "italic" }}
+              >
+                Zap into your dream job
+              </Typography>
+            </Stack>
+          )}
           <Outlet />
-        </Box>
+        </Stack>
       </Stack>
     </Stack>
   );
